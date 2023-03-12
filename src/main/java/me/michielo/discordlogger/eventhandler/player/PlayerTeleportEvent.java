459f01e35f614cc0f84502b516534194e931b79c
Michielo <michiel.kamphuis@hetnet.nl>
2023-03-12 06:58:56 +0100

    added teleport event

---
+++ src/main/java/me/michielo/discordlogger/eventhandler/player/PlayerTeleportEvent.java
@@ -0,0 +1,20 @@
+package me.michielo.discordlogger.eventhandler.player;
+
+import me.michielo.discordlogger.util.Logger;
+import org.bukkit.Location;
+
+public class PlayerTeleportEvent {
+
+    public static void handle(Object event) {
+        org.bukkit.event.player.PlayerTeleportEvent e = (org.bukkit.event.player.PlayerTeleportEvent) event;
+
+        Location from = e.getFrom();
+        Location to = e.getTo();
+
+        String str = e.getPlayer().getName() + " teleported to " + to.getX() + " " + to.getY() + " " + to.getZ() + " from " +
+                from.getX() + " " + to.getY() + " " + to.getZ();
+
+        Logger.logEmbed(str);
+    }
+
+}

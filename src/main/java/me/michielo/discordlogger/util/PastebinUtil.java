e758ad0aad8c93750d5138a7708e10ab3a67ec95
Michielo <michiel.kamphuis@hetnet.nl>
2023-03-12 06:59:24 +0100

    Added pastebin util

---
+++ src/main/java/me/michielo/discordlogger/util/PastebinUtil.java
@@ -0,0 +1,26 @@
+package me.michielo.discordlogger.util;
+
+import com.pastebin.api.Expiration;
+import com.pastebin.api.Format;
+import com.pastebin.api.PastebinClient;
+import com.pastebin.api.Visibility;
+import com.pastebin.api.request.PasteRequest;
+
+public class PastebinUtil {
+
+    public static String createPastebin(String content, String title) {
+        final PastebinClient client = PastebinClient
+                .builder()
+                .developerKey("")
+                .build();
+        final PasteRequest request = PasteRequest
+                .content(content)
+                .visibility(Visibility.UNLISTED)
+                .name(title)
+                .expiration(Expiration.NEVER)
+                .build();
+        // returns the URL of the newly created paste.
+        return client.paste(request);
+    }
+
+}
